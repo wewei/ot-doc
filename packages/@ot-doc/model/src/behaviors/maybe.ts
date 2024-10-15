aefbfe64ba398308c2703794d370e9374f4076fb
Scott Wei <leescott@microsoft.com>
2024-10-15 17:24:19 +0800

    Use symCtor instead of '$' key in Maybe

--- packages/@ot-doc/model/src/behaviors/maybe.ts
+++ packages/@ot-doc/model/src/behaviors/maybe.ts
@@ -1,12 +1,15 @@
 // $: Constructor type
 // v: the value
-export type Nothing = { $: 'N' };
-export type Just<T> = { $: 'J', v: T };
+const symCtor = Symbol();
+export type Nothing = { [symCtor]: 'N' };
+export type Just<T> = { [symCtor]: 'J', v: T };
 export type Maybe<T> = Nothing | Just<T>;
 
-export const nothing = <T>(): Maybe<T> => ({ $: 'N' });
-export const just = <T>(v: T): Maybe<T> => ({ $: 'J', v });
-export const isNothing = <T>(m: Maybe<T>): m is Nothing => m.$ === 'N';
-export const isJust = <T>(m: Maybe<T>): m is Just<T> => m.$ === 'J';
-
-
+export const { nothing, just, isNothing, isJust } = (() => {
+  const NOTHING: Nothing = { [symCtor]: 'N' };
+  const nothing = <T>(): Maybe<T> => NOTHING;
+  const just = <T>(v: T): Maybe<T> => ({ [symCtor]: 'J', v });
+  const isNothing = <T>(m: Maybe<T>): m is Nothing => m === NOTHING;
+  const isJust = <T>(m: Maybe<T>): m is Just<T> => m[symCtor] === 'J';
+  return { nothing, just, isNothing, isJust };
+})();
